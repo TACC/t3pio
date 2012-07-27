e7669094c32458a5350e5eb35b9731db3fc2fa34
Robert McLay <mclay@zaniahgroup.com>
2012-07-27 16:57:39 -0500

    must pass dir thru

--- src/f2c_internal.c
+++ src/f2c_internal.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-#include <t3pio.h>
+#include "t3pio.h"
+#include "t3pio_internal.h"
 #include <mpi.h>
 
 int t3piointernal_(int* f_comm, int* f_info, const char* dir, int* global_size, int* max_stripes, int* factor,
                    const char* file)
 {
-  return t3pio_internal(f_comm, f_info, global_size, max_stripes, factor, file);
+  return t3pio_internal(f_comm, f_info, dir, global_size, max_stripes, factor, file);
 }
 
 int T3PIOINTERNAL(int* f_comm, int* f_info, const char* dir, int* global_size, int* max_stripes, int* factor,
                    const char* file)
 {
-  return t3pio_internal(f_comm, f_info, global_size, max_stripes, factor, file);
+  return t3pio_internal(f_comm, f_info, dir, global_size, max_stripes, factor, file);
 }
 
 int t3piointernal(int* f_comm, int* f_info, const char* dir, int* global_size, int* max_stripes, int* factor,
                    const char* file)
 {
-  return t3pio_internal(f_comm, f_info, global_size, max_stripes, factor, file);
+  return t3pio_internal(f_comm, f_info, dir, global_size, max_stripes, factor, file);
 }
 
 int t3pio_internal(int* f_comm, int* f_info, const char* dir, int* global_size, int* max_stripes, int* factor,
