4d7eed63caac8b53b6d0636f25a86e514206ff8e
Robert McLay <mclay@zaniahgroup.com>
2014-02-22 10:11:55 -0600

    fixed starts problem

--- unstructTest/parallel_io.C
+++ unstructTest/parallel_io.C
@@ -304,10 +304,11 @@ void ParallelIO::MPIIOwriter(CmdLineOptions& cmd)
   gsz[0]    = cmd.xwidth;
   gsz[1]    = cmd.globalSz/cmd.xwidth;
   starts[0] = 0;
-  starts[1] = sz[1]*P.myProc;
+  starts[1] = 0;
     
   ierr = MPI_Type_create_subarray(nDim, sz, sz, starts, MPI_ORDER_C, MPI_DOUBLE, &coreData);
   ierr = MPI_Type_commit(&coreData);
+  starts[1] = sz[1]*P.myProc;
   ierr = MPI_Type_create_subarray(nDim, gsz, sz, starts, MPI_ORDER_C, MPI_DOUBLE, &gblData);
   ierr = MPI_Type_commit(&gblData);
 
